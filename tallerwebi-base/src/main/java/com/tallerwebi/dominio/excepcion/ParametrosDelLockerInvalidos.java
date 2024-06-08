0419cd4737e29b6a52f7bf7cb6223dca0cc1680a
Maximiliano Rabenko <mrabenko@alumno.unlam.edu.ar>
2024-06-08 20:42:33 -0300

    cambios recientes

---
+++ tallerwebi-base/src/main/java/com/tallerwebi/dominio/excepcion/ParametrosDelLockerInvalidos.java
@@ -0,0 +1,8 @@
+package com.tallerwebi.dominio.excepcion;
+
+public class ParametrosDelLockerInvalidos extends RuntimeException{
+
+    public ParametrosDelLockerInvalidos(String message){
+        super(message);
+    }
+}
---
+++ tallerwebi-base/src/main/java/com/tallerwebi/dominio/locker/Haversine.java
@@ -0,0 +1,16 @@
+package com.tallerwebi.dominio.locker;
+
+public class Haversine {
+    public static final double R = 6371; // Radio de la tierra en kilómetros
+
+    public static double distance(double lat1, double lon1, double lat2, double lon2) {
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+                        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return R * c;
+    }
+}
+
